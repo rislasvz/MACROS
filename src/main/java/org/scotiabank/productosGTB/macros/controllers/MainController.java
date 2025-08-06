670c6d3924ba9ff05ca82fac8ff2d72e866311db
AngelNG23 <manunezg@desarrollo-ultrasist.com.mx>
2025-08-06 17:19:07 -0600

    Se agrega fecha actual

--- src/main/java/org/scotiabank/productosGTB/macros/controllers/MainController.java
+++ src/main/java/org/scotiabank/productosGTB/macros/controllers/MainController.java
@@ -1,9 +1,31 @@
 package org.scotiabank.productosGTB.macros.controllers;
 
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
 
+import java.net.URL;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+import java.util.ResourceBundle;
 
+public class MainController implements Initializable {
 
-public class MainController {
+    // Anotación para inyectar el Label desde el FXML
+    @FXML
+    private Label labelFecha;
 
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        mostrarFechaActual();
+    }
 
+    private void mostrarFechaActual() {
+        LocalDate fechaActual = LocalDate.now();
+        Locale locale = new Locale("es", "ES");
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("EEEE dd 'de' MMMM 'del' yyyy", locale);
+        String fechaFormateada = fechaActual.format(formatter);
+        labelFecha.setText(fechaFormateada);
+    }
 }
